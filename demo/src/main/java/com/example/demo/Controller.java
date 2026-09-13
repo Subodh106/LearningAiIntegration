ccc68f497dedff9c69e3fba6cfcc6b8cb8d9eed7
Subodh106 <subodhloves106@gmail.com>
2026-09-13 13:29:30 +0545

    Add Controller class with chat endpoint for handling chat requests

---
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Controller {
+
+    private final Services services;
+
+    @PostMapping("/chat")
+    public String chat(@RequestBody String chat){
+        return services.chat(chat) ;
+    }
+
+}
