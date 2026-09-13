b7492366ecaa6ea94eadf793c634aaed1da4551a
Subodh106 <subodhloves106@gmail.com>
2026-09-13 13:29:45 +0545

    Add SummarizeService class for summarization functionality

---
+++ demo/src/main/java/com/example/demo/summarize/SummarizeService.java
@@ -0,0 +1,19 @@
+package com.example.demo.summarize;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SummarizeService {
+
+    private final ChatClient chatClient;
+
+    public SummarizeService(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    public String summarize(String ticket){
+        return chatClient.prompt().user("Summarize this support ticker :" + ticket).call().content();
+    }
+}
