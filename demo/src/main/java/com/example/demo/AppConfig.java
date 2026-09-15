5bb12b5ecf60e06e08ef3e678663075e43bd51e2
Subodh106 <subodhloves106@gmail.com>
2026-09-15 14:46:06 +0545

    Refactor AppConfig class to enhance configuration setup for RestClient

--- demo/src/main/java/com/example/demo/AppConfig.java
+++ demo/src/main/java/com/example/demo/AppConfig.java
@@ -1,4 +1,16 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+@ComponentScan
 public class AppConfig {
+
+    @Bean
+    public RestClient.Builder restClientBuilder(){
+        return RestClient.builder();
+    }
 }
